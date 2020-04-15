00d9ab5d662b6e5167a2587233250992818af435
Aliagho <ali.bahrami633@yahoo.com>
2020-04-15 17:42:00 +1000

    added Engineer class inherited from Employee

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,23 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+/** This class represents an Engineer */
+class Engineer extends Employee {
+
+    /**
+     * @param {String} name name
+     * @param {Number} id ID
+     * @param {String} email Email Address
+     * @param {String} github Github ID
+     */
+    constructor(name, id, email, github) {
+        super(name, id, email);
+        this.github = github;
+    }
+
+    /** Github ID Getter */
+    getGithub() {
+        return this.github;
+    }
+}
+
+module.exports = Engineer;
