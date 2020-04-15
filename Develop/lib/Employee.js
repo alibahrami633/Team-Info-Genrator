4162e91b25eb0eb60a6ffa537b1e638a2bb91ac9
Aliagho <ali.bahrami633@yahoo.com>
2020-04-15 17:39:21 +1000

    added Employee class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,38 @@
-// TODO: Write code to define and export the Employee class
+class Employee {
+
+    /**
+     * Creates an instance of an Employee
+     * @param {String} name Employee's name
+     * @param {Number} id Employee's Id
+     * @param {String} email Employee's email address
+     */
+
+    constructor(name, id, email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.role = "Employee";
+    }
+
+    /** Email getter */
+    getEmail() {
+        return this.email;
+    }
+
+    /** Role Getter */
+    getRole() {
+        return this.role;
+    }
+
+    /** ID getter */
+    getId() {
+        return this.id;
+    }
+
+    /** Name Getter */
+    getName() {
+        return this.name;
+    }
+}
+
+module.exports = Employee;
